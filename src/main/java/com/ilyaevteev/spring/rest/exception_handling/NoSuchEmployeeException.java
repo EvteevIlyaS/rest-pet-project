e4f52a8ad26e1b5a9b377d26fc8913cac1165a9d
Ilya Evteev <evteev-ilya@inbox.ru>
2023-03-06 00:44:08 +0300

    first commit

---
+++ src/main/java/com/ilyaevteev/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.ilyaevteev.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
